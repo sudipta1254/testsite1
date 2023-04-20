3e6f64539b584e08805eb3517c06ef18abc46dc2
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2023-04-20 18:43:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ t = new Date().getFullYear();
 const w = screen.width, h = screen.height;
 /* document.getElementById('yr').innerHTML = t;
 alert('Width: '+w+' Height: '+h); */
-document.getElementsByTagName('body').style.backgroundImage = 'url(https://source.unsplash.com/random/'+w+'x'+h+'?nightlife,neondemon,purrple,streetnight,city,night)';
+document.body.style.backgroundImage = 'url(https://source.unsplash.com/random/'+w+'x'+h+'?nightlife,neondemon,purrple,streetnight,city,night)';
 
