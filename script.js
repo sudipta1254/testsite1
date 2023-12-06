6ef71239e21637ba6d5d0f36307b48e58b4564a3
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2023-12-06 09:14:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
-t = new Date().getFullYear();
-const w = screen.width, h = screen.height;
-/* document.getElementById('yr').innerHTML = t; 
-alert('Width: '+w+' Height: '+h); 
-document.body.style.backgroundImage = 'url(https://source.unsplash.com/random/'+w+'x'+h+'?nightlife,neondemon,purrple,streetnight,city,night)';*/
-c1 = document.getElementById('c1');
+let list = 'uuee cyvm egll lfpg zbaa eham ulaa engm eidw panc ellx loww lshz ebbr eddf cyyz eddb eddk uaaa eprz lipz efhk bikf visr', inp = $('input'), ol = $('#fill'), sn = '';
 
-//c1.style.background = 'url(https://source.unsplash.com/random/'+w+'x'+h+'?nightlife,neondemon,purrple,streetnight,city,nightnight)';
+$('button').click(function() {
+   let id = list.toUpperCase().split(' ');
+   if(inp.val())
+      get(inp.val().trim());
+    else
+      for(let i = 0; i < id.length; i++)
+         setTimeout(get(id[i], i), 3000);
+   if(!sn)
+      ol.text('No airport found');
+})
+function get(id, i = 0) {
+      let url = `https://cors-anywhere.herokuapp.com/https://aviationweather.gov/api/data/metar?ids=${id}&format=json`;
+      fetch(url)
+      .then(res => {
+         if(!res.ok)
+            if(i == 0)
+               alert('AWC error: '+res.status+' - '+res.type);
+            else
+               console.log(res.status+' - '+res.type);
+         return res.json();
+      })
+      .then(data => {
+         let dt = data[0];
+         if(sn = dt.wxString.includes('SN')) {
+            if(i == 0)
+               ol.empty();
+            ol.append(`<li>${dt.name}  -->  ${dt.wxString}</li>`);
+         }
+         // console.log((i+1)+' - '+dt.name+' '+dt.wxString);
+      });
+}
 
+$('input').keypress(function(event) {
+  if(event.key === "Enter") {
+    event.preventDefault();
+    $('button').click();
+  }
+});
 
+// $('input').on('input', function() {
+//    $console.log($(this).val().length);
+// })
