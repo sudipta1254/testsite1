66702104946a6f728ab28b66ee1d0032036f593c
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2023-12-06 10:41:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let list = 'uuee cyvm egll lfpg zbaa eham ulaa engm eidw panc ellx loww lshz ebbr eddf cyyz eddb eddk uaaa eprz lipz efhk bikf visr', inp = $('input'), ol = $('#fill'), sn = '';
+let list = 'uuee cyvm egll lfpg uacc zbaa eham ulaa engm eidw panc ellx loww lshz ebbr eddf cyyz eddb eddk uaaa eprz lipz efhk bikf visr', inp = $('input'), ol = $('#fill'), sn = '';
 
 $('button').click(function() {
    inp.blur();
