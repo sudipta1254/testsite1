6b47bc5c545d0a0fdb6cd9bbf4fd4896515ef530
Sudipta Singha <singhaashoka03@gmail.com>
2024-06-24 12:58:44 +0530

    Add localStorage feature to store data in user's device

--- src/App.js
+++ src/App.js
@@ -4,21 +4,31 @@ import AddTodo from "./AddTodo"
 
 class App extends Component {
   state = {
-    todos: [
-      {id: 1, content: 'buy milk'},
-      {id: 2, content: 'play game'}
-    ]
+    todos: []
+  }
+  componentDidMount() {
+    this.setState({
+      todos: this.getSetTodo(0, 0)
+    })
   }
   deleteTodo = (id) => {
     const todos = this.state.todos.filter(todo => {
       return todo.id !== id
     })
     this.setState({ todos })
+    this.getSetTodo(todos, 1)
   }
   addTodo = (todo) => {
     todo.id = Math.random()
     let todos = [...this.state.todos, todo]
     this.setState({ todos })
+    this.getSetTodo(todos, 1)
+  }
+  getSetTodo = (item, id = 0) => {
+    if (id)
+      localStorage.setItem('todos', JSON.stringify(item))
+    else
+      return JSON.parse(localStorage.getItem('todos'))
   }
   render() {
     return (
