f895f25e2d9b4658221962590be38ad53f705684
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2024-07-03 21:16:37 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import "./index.css";
 import $ from "jquery"
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
 
 const root = $('#root')[0]
 createRoot(root).render(
