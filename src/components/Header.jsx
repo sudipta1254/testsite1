51d38d7c3aac9e6a60c36ba3a23f46d74250169e
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2024-07-03 13:50:34 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,11 @@ const flex = {display:"flex",alignItems:"center",justifyContent:"space-between"}
 
 const Header = () => {
    return (
-      <Router>
+      {/*<Router>*/}
          <header className='header blue lighten-4 green-text text-darken-2' style={flex}>
             <div className="app-logo-text">
                <img src={icon1} alt="app logo" />
-               <p><a href="/">Flights</a></p>
+               <p><Link to="/">Flights</Link></p>
             </div>
             <div>
                <a href='#!' data-target="slide-nav" className="sidenav-trigger">
@@ -18,12 +18,12 @@ const Header = () => {
                </a>
             </div>
             <ul className="sidenav" id="slide-nav">
-               <li><a className="subheader" href="#!">Main options</a></li>
-               <li><a className="" href="/">Realtime</a></li>
-               <li><a className="" href="/info">Information</a></li>
+               <li><Link className="subheader" to="#!">Main options</Link></li>
+               <li><Link className="" to="/">Realtime</Link></li>
+               <li><Link className="" to="/info">Information</Link></li>
             </ul>
          </header>
-      </Router>
+      {/*<Router>*/}
    );
 }
 
